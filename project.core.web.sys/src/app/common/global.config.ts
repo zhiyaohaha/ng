686f8c52bee152bff60940ef07f92b437bb3c859
tangchengli <xiaoxiezhimu@qq.com>
2017-11-13 15:28:42 +0800

    默认图片地址

--- project.core.web.sys/src/app/common/global.config.ts
+++ project.core.web.sys/src/app/common/global.config.ts
@@ -3,20 +3,24 @@
 // }
 
 export const globalVar = {
-    pageSizes: [10, 30, 50, 100, 200],//可选的每页数量
-    pageSize: 10,//每页条数
-    pageLinkCount: 5,//显示多少页码
-}
+  pageSizes: [10, 30, 50, 100, 200], //可选的每页数量
+  pageSize: 10, //每页条数
+  pageLinkCount: 5, //显示多少页码
+};
 
 export const customized = {
     SysParam: "SysParam",
     SysArea: "SysArea",
     SysParamDetail: "SysParamDetail",
     SysOperationLogConfig: "SysOperationLogConfig"
-}
+};
 
 export const globalUrl = {
     private_key: "84qudMIhOkX5JMQXVd0f4jneqfP2Lp",
     baseUrl: "http://api.cpf360.com",
     wsUrl: "ws://120.26.39.240:8181/"
-}
+};
+
+export const defaultValue = {
+  imgSrc: "http://data.cpf360.com/default/default.jpg"
+};
