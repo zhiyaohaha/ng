f4738f170eacaf97588ddef6d04a52a747bc8857
tangchengli <xiaoxiezhimu@qq.com>
2017-11-17 14:04:33 +0800

    增加二维码显示

--- project.core.web.sys/src/app/component/table/table.component.ts
+++ project.core.web.sys/src/app/component/table/table.component.ts
@@ -1,13 +1,33 @@
-import { Router } from "@angular/router";
-import { CommonModule } from "@angular/common";
+import {Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
 import {MdSelectModule} from "@angular/material";
-import { ITdDataTableColumn, CovalentDataTableModule, CovalentPagingModule, IPageChangeEvent } from "@covalent/core";
-import {
-  NgModule, Component, OnInit, Input, Output, ViewChild, ViewChildren,
-  ContentChildren, ElementRef, QueryList, EventEmitter, Renderer2, TemplateRef, ContentChild,
-} from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { globalVar } from "./../../common/global.config";
+import {CovalentDataTableModule, CovalentPagingModule, IPageChangeEvent} from "@covalent/core";
+import {Component, ElementRef, EventEmitter, Input, NgModule, OnInit, Output, ViewChild,} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {globalVar} from "./../../common/global.config";
+import {TableColumns} from "../../common/interface/table-columns";
+import {ConvertUtil} from "../../common/convert-util";
+
+const DATE_FORMART: (v: any) => any = v => {
+  let date = new Date(v);
+
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  return h + ":" + m + ":" + s;
+};
+const DATE_TIME_FORMART: (v: any) => any = v => {
+  let date = new Date(v);
+
+  let Y = date.getFullYear();
+  let M = date.getMonth() + 1;
+  let D = date.getDate();
+
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  return Y + "-" + M + "-" + D + " " + h + ":" + m + ":" + s;
+};
 
 @Component({
   selector: "app-table",
@@ -19,7 +39,32 @@ export class TableComponent implements OnInit {
 
   @Input() clickAuthority; // 点击权限
   @Input() data; // 表格数据内容
-  @Input() columns: ITdDataTableColumn[]; // 表头
+  // @Input() columns: ITdDataTableColumn[]; // 表头
+  columnsPipes = {}; //有管道的列
+  @Input()
+  get columns() {
+    return this._columns;
+  }
+  set columns(value) {
+    if (Array.isArray(value)) {
+      value.forEach(item => {
+        switch (item.pipe) {
+          case "HtmlPipe.Date": item.format = DATE_FORMART;
+            break;
+          case "HtmlPipe.DateTime": item.format = DATE_TIME_FORMART;
+            break;
+          default :
+            break;
+        }
+        if (item.pipe) {
+          this.columnsPipes[item.name] = item.pipe;
+        }
+      });
+    }
+    this._columns = value;
+  }
+
+  _columns: TableColumns[];
   @Input() totals; // 总条数
   @Input() pageSize = globalVar.pageSize; // 每页显示的条数
   @Input() pageSizes = globalVar.pageSizes; // 可选的每页条数
@@ -31,7 +76,9 @@ export class TableComponent implements OnInit {
   @ViewChild("pagingBar")
   private pagingBar: ElementRef;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private convertUtil: ConvertUtil
+  ) { }
   ngOnInit() { }
 
   /**
