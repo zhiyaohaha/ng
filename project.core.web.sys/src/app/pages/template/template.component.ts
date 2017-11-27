038a984e3d0e95edbdf39f97c66e1597afc7e620
tangchengli <xiaoxiezhimu@qq.com>
2017-11-27 20:21:26 +0800

    template

---
+++ project.core.web.sys/src/app/pages/template/template.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.scss']
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() {
+    alert(111)
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ project.core.web.sys/src/app/pages/template/template.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TemplateComponent} from './template.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TemplateComponent]
+})
+export class TemplateModule { }
