a01709ff76b1256cd5155dadc2fac274e7480001
tangchengli <xiaoxiezhimu@qq.com>
2017-12-15 16:39:44 +0800

    修复详情组件，有的数据无法展示

--- project.core.web.sys/src/app/common/pipe/boolean-to-word.pipe.ts
+++ project.core.web.sys/src/app/common/pipe/boolean-to-word.pipe.ts
@@ -6,10 +6,20 @@ import {Pipe, PipeTransform} from "@angular/core";
 export class BooleanToWordPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if (value) {
-      return "是";
+    if (value.indexOf(".") > -1) {
+      let arr = value.split(".");
+      for (let i = 0; i < arr.length; i++) {
+        if (!args[arr[i]]) {
+          return "否";
+        } else {
+          args = args[arr[i]];
+          if (typeof args === "boolean") {
+              return "是";
+          }
+        }
+      }
     } else {
-      return "否";
+      return args[value];
     }
   }
 
