7ee7ce5ae2169ca218d633b8d9184048d917c993
zhangdingsong <1050296225@qq.com>
2018-01-24 16:38:00 +0800

    补资料页面，上传附件实时 修改附件项/组状态

--- project.core.web.sys/src/app/newcomponent/application/application.component.ts
+++ project.core.web.sys/src/app/newcomponent/application/application.component.ts
@@ -113,24 +113,36 @@ export class ApplicationComponent extends BaseUIComponent implements OnInit {
   //接收来自 上传组件 上传的最新文件数据。  更新当前组件的数据
   onPostFileData($event, i, k) {
     let data = this.loanInfo._attachmentGroups;
+
     data.forEach((item1, index1) => {
       if (index1 == i) {
         item1._attachments.forEach((item2, index2) => {
-          // console.log(item2)
+          console.log(item2)
           if (item2 == k) {
             let res = item2._files;
             if (res) {
-              item2._files = res.concat($event);
+              item2._files = res.concat($event._files[0]);
             } else {
-              item2._files = $event;
+              item2._files = [];
+              item2._files[0] = $event._files[0];
             }
+            item2._status = this.stausCodeLabel($event._status);
           }
         })
+        item1._status = this.stausCodeLabel($event.groupStatus);
       }
     })
   }
 
-
+  //根据状态码，修改状态label
+  stausCodeLabel(code) {
+    let label;
+    switch (code) {
+      case "LoanOrderAttachmentStatus.Audit": label = "待审核"; break;
+      case "LoanOrderAttachmentStatus.Uncommitted": label = "待填写"; break;
+    }
+    return label;
+  }
 
   //动态表单数据
   onSubmitParams($event) {
