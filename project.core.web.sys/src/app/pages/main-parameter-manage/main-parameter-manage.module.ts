700f66444f35f457d97ffb441f18fab84b5467df
limeng <1813933328@qq.com>
2018-02-03 19:04:10 +0800

    数据模板

--- project.core.web.sys/src/app/pages/main-parameter-manage/main-parameter-manage.module.ts
+++ project.core.web.sys/src/app/pages/main-parameter-manage/main-parameter-manage.module.ts
@@ -15,24 +15,44 @@ import { ChipModule } from "../../component/chip/chip.component";
 import { SearchFormModule } from "../../component/search-form/searchform.component";
 import { TableModule } from "../../component/table/table.component";
 import { ResponsiveModelModule } from "./../../component/responsive-model/responsive-model.component";
-import {TimiFileUploaderModule} from "../../component/timi-ng2-file-uploader/timi-ng2-file-uploader.component";
-import {TimiInputModule} from "../../component/timi-input/timi-input.component";
-import {TimiChipModule} from "../../component/timi-chip/chip.component";
-import {TimiTextareaModule} from "../../component/timi-textarea/timi-textarea.component";
-import {TimiTableModule} from "../../component/timi-table/timi-table.component";
+import { TimiFileUploaderModule } from "../../component/timi-ng2-file-uploader/timi-ng2-file-uploader.component";
+import { TimiInputModule } from "../../component/timi-input/timi-input.component";
+import { TimiChipModule } from "../../component/timi-chip/chip.component";
+import { TimiTextareaModule } from "../../component/timi-textarea/timi-textarea.component";
+import { TimiTableModule } from "../../component/timi-table/timi-table.component";
+import { DetailModelModule } from "../../component/detail-model/detail-model.component";
+import { NewComponentModule } from "app/newcomponent/newcomponent.module";
+
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule, ReactiveFormsModule,
+    FormsModule, 
+    ReactiveFormsModule,
     MainParameterManageRoutingModule,
-    MdInputModule, MdIconModule, MdSelectModule, MdButtonModule, MdSidenavModule, MdDatepickerModule, MdChipsModule, MdTabsModule,
-    CovalentPagingModule, CovalentChipsModule,
+    MdInputModule, 
+    MdIconModule, 
+    MdSelectModule, 
+    MdButtonModule, 
+    MdSidenavModule, 
+    MdDatepickerModule, 
+    MdChipsModule, 
+    MdTabsModule,
+    CovalentPagingModule, 
+    CovalentChipsModule,
     TreeModule,
     ButtonModule,
     ChipModule,
     SearchFormModule,
-    TableModule, ResponsiveModelModule, TimiFileUploaderModule, TimiInputModule, TimiChipModule, TimiTextareaModule, TimiTableModule
+    TableModule, 
+    ResponsiveModelModule, 
+    TimiFileUploaderModule, 
+    TimiInputModule, 
+    TimiChipModule, 
+    TimiTextareaModule, 
+    TimiTableModule, 
+    DetailModelModule,
+    NewComponentModule
   ],
   declarations: [
     MainParameterManageComponent
