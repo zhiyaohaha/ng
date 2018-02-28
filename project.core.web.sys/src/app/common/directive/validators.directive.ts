5ec1d0c8992ffb888c76c2ca7de69d98a7a79144
zhangdingsong <admin@DESKTOP-EETU631>
2018-02-28 11:02:24 +0800

    -

--- project.core.web.sys/src/app/common/directive/validators.directive.ts
+++ project.core.web.sys/src/app/common/directive/validators.directive.ts
@@ -60,15 +60,21 @@ export class CustomValidatorsDirective implements OnInit {
   }
   //select 
   @HostListener('onChange', ['$event']) onChangeSelect(e) {
-    //日历组件，在刚刚加载的时候就触发了onchange事件，所以要区分一下
-    if (!e['year']) {  //日历在刚刚加载触发onchange事件的时候，不触发验证事件。
-      this.firstEnterView = false;
-      this.verifyInputValue(e, "required", '必选');
-    } else {  //日历在修改以后，再次触发onchange会,触发验证事件
-      if (e['value']) {
+
+    if (e) {
+      //日历组件，在刚刚加载的时候就触发了onchange事件，所以要区分一下
+      if (!e['year']) {  //日历在刚刚加载触发onchange事件的时候，不触发验证事件。
         this.firstEnterView = false;
         this.verifyInputValue(e, "required", '必选');
+      } else {  //日历在修改以后，再次触发onchange会,触发验证事件
+        if (e['value']) {
+          this.firstEnterView = false;
+          this.verifyInputValue(e, "required", '必选');
+        }
       }
+    } else {
+      this.firstEnterView = false;
+      this.verifyInputValue(e, "required", '必选');
     }
 
   }
@@ -162,7 +168,6 @@ export class CustomValidatorsDirective implements OnInit {
    * @memberof CustomValidatorsDirective
    */
   errorTipsDisplay(msg, errTipsDisplay) {
-    console.log(errTipsDisplay)
     if (errTipsDisplay) {  //显示
       //组件和errtips的共同父类样式
       let _self = this;
@@ -213,7 +218,8 @@ export class CustomValidatorsDirective implements OnInit {
       case "file-uploader": componentParentClass = "errBorderParentFileUpLoad"; break;
       case "textarea": componentParentClass = "errBorderParentTextArea"; break;
       case "tags": componentParentClass = "errBorderParentTags"; break;
-      case "free-calendar": componentParentClass = "errBorderParentCalendar"; break;
+      case "free-calendar-date": componentParentClass = "errBorderParentCalendarDate"; break;
+      case "free-calendar-time": componentParentClass = "errBorderParentCalendarTime"; break;
       default: componentParentClass = "errBorderParent"; break;  //例如input
     }
     // console.log(componentParentClass)
