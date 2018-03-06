3657b83ec44dcca04b0ac5804fdac42bf3d9e730
tangchengli <xiaoxiezhimu@qq.com>
2018-03-06 15:08:26 +0800

    给输入框添加单位显示

--- project.core.web.sys/src/app/component/timi-input/timi-input.component.ts
+++ project.core.web.sys/src/app/component/timi-input/timi-input.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from "@angular/common";
+import {CommonModule} from "@angular/common";
 import {
   AfterViewInit,
   Component,
@@ -12,8 +12,8 @@ import {
   Renderer2,
   ViewChild
 } from "@angular/core";
-import { DomRenderer } from "../../common/dom";
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import {DomRenderer} from "../../common/dom";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 export const TIMI_INPUT_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -29,8 +29,10 @@ export const TIMI_INPUT_VALUE_ACCESSOR: any = {
       <div class="box-item item-control-wrapper">
         <div #wrap class="item-control">
           <input #input class="item-input" type="{{type}}" placeholder="{{placeholder}}" [disabled]="disabledVal"
-                 name="{{name}}" value="{{value}}" (blur)="onBlur($event)" spellcheck="false" autocomplete="off"
+                 name="{{name}}" value="{{value}}" (blur)="onBlur($event)" (change)="onChange($event)"
+                 spellcheck="false" autocomplete="off" [ngStyle]="{'width': inputWidth}"
                  required="{{require}}" (dragover)="allowDrop($event)" (drop)="drop($event)" (focus)="onFocus($event)">
+          <span class="unit">{{unit}}</span>
           <span class="item-error-tip">{{errorTips}}</span>
         </div>
       </div>
@@ -40,6 +42,7 @@ export const TIMI_INPUT_VALUE_ACCESSOR: any = {
 })
 export class TimiInputComponent implements ControlValueAccessor, AfterViewInit, OnInit {
 
+  unit: string; // 单位
   @Input()
   get value() {
     return this._value;
@@ -47,7 +50,7 @@ export class TimiInputComponent implements ControlValueAccessor, AfterViewInit, 
 
   set value(value) {
     this._value = value;
-    this.valueChange(this._value);
+    this.valueChange(this.returnValue(this._value));
   }
 
   _value;
@@ -55,25 +58,37 @@ export class TimiInputComponent implements ControlValueAccessor, AfterViewInit, 
   @Input() labelWidth: string;
   @Input() labelName: string;
   @Input() name: string;
-  @Input() inputWidth: string;
+  @Input() inputWidth: string = "100%";
   @Input() placeholder: string;
   @Input() columns: number;
   @Input() disabledVal: boolean;
   @Input() require: boolean;
   @Input() pattern: string;
   @Input() errorTips: string;
+  @Input() // 单位的管道
+  set unitPipe(value) {
+    this.inputWidth = value ? "" : "100%";
+    this.unit = this.switchUnit(value);
+    this._unitPipe = value;
+  }
+  get unitPipe() {
+    return this._unitPipe;
+  }
+  _unitPipe: string;
 
   @Output() blur: EventEmitter<any> = new EventEmitter();
   @Output() focus: EventEmitter<any> = new EventEmitter();
-  
-  
+  @Output() change: EventEmitter<any> = new EventEmitter();
+
+
   @ViewChild("wrap")
   wrapRef: ElementRef;
 
   @ViewChild("input")
   inputRef: ElementRef;
 
-  private valueChange = (_: any) => { };
+  private valueChange = (_: any) => {
+  };
 
   constructor(private renderer: Renderer2) {
   }
@@ -125,12 +140,17 @@ export class TimiInputComponent implements ControlValueAccessor, AfterViewInit, 
     } else {
       this.renderer.addClass(this.wrapRef.nativeElement, "error");
     }
-    this.valueChange(this._value);
+    // this.valueChange(this.returnValue(this._value));
   }
 
   // 失去焦点的事件
-  onFocus(e){
+  onFocus(e) {
     this.focus.emit(e);
+  }
+
+  // change
+  onChange($event) {
+    this.change.emit($event);
   }
 
   /*允许拖放*/
@@ -158,12 +178,73 @@ export class TimiInputComponent implements ControlValueAccessor, AfterViewInit, 
     }
   }
 
+  /**
+   * 根据管道获取单位
+   */
+  switchUnit(value: string): string {
+    let text = "";
+    switch (value){
+      case "HtmlPipe.M2":
+        text = "m²";
+        break;
+      case "HtmlPipe.Day":
+        text = "天";
+        break;
+      case "HtmlPipe.InterestRate":
+        text = "%";
+        break;
+      case "HtmlPipe.Individual":
+        text = "个";
+        break;
+      case "HtmlPipe.Element":
+        text = "元";
+        break;
+      case "HtmlPipe.TenThousandElement":
+        text = "万元";
+        break;
+      case "HtmlPipe.Age":
+        text = "岁";
+        break;
+      case "HtmlPipe.TenThousandKM":
+        text = "万公里";
+        break;
+      case "":
+        text = "月";
+        break;
+      default:
+        break;
+    }
+    return text;
+  }
+
+  /**
+   * 根据管道返回计算过后的值
+   */
+  returnValue(value: any): any {
+    let text;
+    switch (this.unitPipe) {
+      case "HtmlPipe.InterestRate":
+        text = value / 100;
+        break;
+      case "HtmlPipe.TenThousandKM":
+        text = value * 10000;
+        break;
+      case "HtmlPipe.TenThousandElement":
+        text = value * 10000;
+        break;
+      default:
+        text = value;
+        break;
+    }
+    return text;
+  }
+
   writeValue(obj: any): void {
     // this.value = obj || '';
-    if (obj == null || obj == undefined) {
-      this.value = '';
+    if (obj == null || obj === undefined) {
+      this.value = "";
     } else {
-      this.value = obj
+      this.value = obj;
     }
   }
 
