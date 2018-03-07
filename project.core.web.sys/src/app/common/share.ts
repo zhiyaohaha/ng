353f7a149cab07ef50f559c4280d544932f5071a
tangchengli <xiaoxiezhimu@qq.com>
2018-03-07 16:22:05 +0800

    添加新功能

--- project.core.web.sys/src/app/common/share.ts
+++ project.core.web.sys/src/app/common/share.ts
@@ -1,51 +1,100 @@
-import { CommonModule } from "@angular/common";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {CommonModule} from '@angular/common';
 import {
   NgModule, Component, ViewContainerRef, EmbeddedViewRef, Input, OnInit, Directive,
-  ComponentFactoryResolver
-} from "@angular/core";
+  ComponentFactoryResolver, OnDestroy, TemplateRef
+} from '@angular/core';
 
 @Component({
-  selector: "f-header",
-  template: "<ng-content></ng-content>"
+  selector: 'f-header',
+  template: '<ng-content></ng-content>'
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+}
 
 @Component({
-  selector: "f-footer",
-  template: "<ng-content></ng-content>"
+  selector: 'f-footer',
+  template: '<ng-content></ng-content>'
 })
 
-export class FooterComponent {}
+export class FooterComponent {
+}
 
 @Component({
-  selector: "free-template",
+  selector: 'free-template',
   template: ``
 })
 
-export class TemplateComponent implements OnInit {
+export class TemplateComponent implements OnInit, OnDestroy {
 
   @Input() template: any;
+  @Input() index: any;
+  @Input() data: any;
   view: EmbeddedViewRef<any>;
 
-  constructor(public _viewContainerRef: ViewContainerRef) {}
+  constructor(public _viewContainerRef: ViewContainerRef) {
+  }
 
   ngOnInit() {
-    this.view = this._viewContainerRef.createEmbeddedView(this.template, {});
+    if (this.template) {
+      this.view = this._viewContainerRef.createEmbeddedView(this.template, {
+        '\$implicit': this.data,
+        'index': this.index
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.view) {
+      this.view.destroy();
+    }
+  }
+}
+
+@Component({
+  selector: 'free-column-template',
+  template: ``
+})
+
+export class TemplateColumnComponent implements OnInit, OnDestroy {
+
+  @Input() template: any;
+  @Input() rowData: any;
+  @Input() rowIndex: number;
+  @Input() column: any;
+  view: EmbeddedViewRef<any>;
+
+  constructor(public _viewContainerRef: ViewContainerRef) {
+  }
+
+  ngOnInit() {
+    if (this.template) {
+      this.view = this._viewContainerRef.createEmbeddedView(this.template, {
+        '\$implicit': this.column,
+        'rowData': this.rowData,
+        'rowIndex': this.rowIndex
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.view) {
+      this.view.destroy();
+    }
   }
 }
 
 @Directive({
-  selector: "[fHost]"
+  selector: '[fHost]'
 })
 
 export class FHostDirective implements OnInit {
 
   @Input() item: any;
   @Input() data: any;
-  constructor(private _viewContainerRef: ViewContainerRef,
-              private _componentFactoryResolver: ComponentFactoryResolver) {
+
+  constructor(public _viewContainerRef: ViewContainerRef,
+              public _componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   ngOnInit() {
@@ -59,10 +108,30 @@ export class FHostDirective implements OnInit {
   }
 }
 
+@Directive({
+  selector: '[fTemplate]'
+})
+export class FreeTemplateDirective {
+  @Input() type: string;
+  @Input('fTemplate') name: string;
+
+  constructor(public template: TemplateRef<any>) {
+  }
+
+  getType(): string {
+    return this.name;
+  }
+}
+
 @NgModule({
   imports: [CommonModule],
-  declarations: [ HeaderComponent, FooterComponent, TemplateComponent, FHostDirective ],
-  exports: [HeaderComponent, FooterComponent, TemplateComponent, FHostDirective]
+  declarations: [
+    HeaderComponent, FooterComponent, TemplateComponent,
+    TemplateColumnComponent, FHostDirective, FreeTemplateDirective],
+  exports: [
+    HeaderComponent, FooterComponent, TemplateComponent,
+    TemplateColumnComponent, FHostDirective, FreeTemplateDirective]
 })
 
-export class ShareModule {}
+export class ShareModule {
+}
